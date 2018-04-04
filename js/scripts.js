b1d521f253e030e8b89efcfeee33efa3d7defb08
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-04-04 15:59:53 +0200

    Change getQuote function

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var prefix = "https://cors-anywhere.herokuapp.com/";
 
 function getQuote() {
-    $.getJSON(quoteUrl, createTweet);
+    $.getJSON(prefix + quoteUrl, createTweet);
+    $.ajaxSetup({ cache: false });
 }
+
 function createTweet(input) {
     var data = input[0];
 
@@ -14,13 +17,14 @@ function createTweet(input) {
         quoteAuthor = "Unknown author";
     }
     var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
     if (tweetText.length > 140) {
     getQuote();
     } else {
-    var tweet = tweetLink + encodeURIComponent(tweetText);
-    $('.quote').text(quoteText);
-    $('.author').text("Author: " + quoteAuthor);
-    $('.tweet').attr('href', tweet);
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        $('.quote').text(quoteText);
+        $('.author').text("Author: " + quoteAuthor);
+        $('.tweet').attr('href', tweet);
     }
 }
 $(document).ready(function() {
